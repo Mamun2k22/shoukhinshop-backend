6e4e25fcf3d172611835b8ddcabf0d70af568e2b
Mamun2k22 <mamun180037@gmail.com>
2026-04-10 21:39:38 +0600

    fix auth

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,49 +1,10 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { User } from "../model/index.model.js";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
-
-        if (!user) {
-          // Create a new user if not found
-          user = new User({
-            googleId: profile.id,
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            profilePicture: profile.photos[0].value,
-            isVerified: true, // Google accounts are considered verified
-          });
-
-          await user.save();
-        }
-
-        done(null, user);
-      } catch (error) {
-        console.error("Google Auth Error:", error);
-        done(error, null);
-      }
-    }
-  )
-);
-
-// 🔹 Serialize user
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-// 🔹 Deserialize user
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
@@ -53,4 +14,4 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-
+export default passport;
